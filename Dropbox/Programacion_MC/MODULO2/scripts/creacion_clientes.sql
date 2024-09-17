391d9ddae4a809e28e7e0ae529432b7e22c77237
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-17 17:05:44 -0500

    ejercicio crear tabla clientes

---
+++ Dropbox/Programacion_MC/MODULO2/scripts/creacion_clientes.sql
@@ -0,0 +1,10 @@
+-- crea tabla clientes
+drop table clientes
+
+create table clientes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	edad int,
+	constraint clientes_pk primary key (cedula)
+)
