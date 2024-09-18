c6c89664fed0408d787bde941ac3d34a83b3053f
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-18 17:07:47 -0500

    se agregan 8 inserts

--- scripts/estudiantes.sql
+++ scripts/estudiantes.sql
@@ -9,3 +9,28 @@ create table estudiantes(
 	fecha_nacimiento date not null,
 	constraint estudiantes_pk primary key (cedula)
 )
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721730131','Cristina','Villarreal','crisvill@gmail.com','1999/10/02')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721735632','Vinicio','Castro','vinicastro@gmail.com','2001/02/06')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1718198938','Eduardo','Villacres','edu87@gmail.com','2000/01/18')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721734785','Rocío','Iza','rocio_iza@gmail.com','2002/05/25')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721730132','Antonio','Chicaiza','antonych@gmail.com','2000/11/02')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721778523','Elias','Andrade','elias_an@gmail.com','1999/03/01')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1721735236','Marlene','Aldáz','aldaz_mar@gmail.com','2001/12/25')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values ('1704080080','Leonidas','Panama','leo_pan@gmail.com','2002/07/08')
+
+select * from estudiantes
