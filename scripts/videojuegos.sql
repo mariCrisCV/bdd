f589ff217d87686d2389f3bedfce793e85ec1851
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-18 17:24:13 -0500

    se agregan 5 inserts con todos los campos

--- scripts/videojuegos.sql
+++ scripts/videojuegos.sql
@@ -8,3 +8,17 @@ create table videojuegos(
 	valoracion int not null,
 	constraint videojuegos_pk primary key (codigo)
 )
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('111','The Legend of Zelda','accion',9)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('112','Tony Hawks Pro Skater','accion',8)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('113','Grand Theft Auto','accion',7)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('114','Soul Calibur','accion',8)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values ('115','Super Mario Galaxi','Wii',9)
