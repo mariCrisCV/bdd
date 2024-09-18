5b16dd030a0988b84441d423f3f2ae3b24b2febb
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-18 12:52:48 -0500

    se crean 5 inserts con todos los campos de tabla productos

--- scripts/productos.sql
+++ scripts/productos.sql
@@ -9,3 +9,17 @@ create table productos(
 	stock int not null,
 	constraint productos_pk primary key (codigo)
 )
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('5639','Shampoo','producto de aseo',8.75,22)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('8963','Jabon','producto de aseo personal',3.75,50)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('7452','Crema corporal','producto de cuidado personal',5.80,16)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('7854','Desodorante','producto de cuidado personal',4.95,41)
+
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values ('7863','Acondicionador','producto de cuidado personal',7.95,32)
