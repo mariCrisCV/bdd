189a1344aa70c1ff4c9cdeef958eaff260ba863f
mariCrisCV <maria.cris.castaneda.v@gmail.com>
2024-09-18 21:53:42 -0500

    se crean selecciones en registros_entrada

--- scripts/registros_entrada.sql
+++ scripts/registros_entrada.sql
@@ -39,3 +39,7 @@ insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
 values ('258','1721730362','2024/09/16','06:57')
 
 select * from registros_entrada
+
+select cedula_empleado,fecha,hora from registros_entrada
+select * from registros_entrada where hora between '7:00' and '14:00'
+select * from registros_entrada where hora > '8:00'
